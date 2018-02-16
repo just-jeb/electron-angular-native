ec9f920c67b014c9125102d205fb7be989fedd70
Evgeny Barabanov <meltedspark@gmail.com>
2018-02-16 21:29:39 +0200

    Wait until window is loaded before accessing elements

--- e2e/native-strings.e2e-spec.ts
+++ e2e/native-strings.e2e-spec.ts
@@ -9,7 +9,7 @@ chai.use(chaiAsPromised);
 
 function containsString(app: Application, str: string) {
   it(`Contains ${str}`,
-    () => app.client.getText('ul li h2').then(list => {
+    () => app.client.waitUntilWindowLoaded().getText('ul li h2').then(list => {
       const index = list.indexOf(str);
       return index > -1 ? list[index] : '';
     }).should.eventually.equal(str)
@@ -17,6 +17,9 @@ function containsString(app: Application, str: string) {
 }
 
 SpectronUtils.describe('Native strings', app => {
+
   containsString(app, 'c++ .node string');
   containsString(app, 'c++ precompiled library string');
+  // it('Save page', () => app.webContents.savePage(path.resolve('./page.html'), 'HTMLComplete'));
+
 });
