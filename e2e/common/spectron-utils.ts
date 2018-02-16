155f8479847a87c9c3d51977a4f711b26cd2bc91
Evgeny Barabanov <meltedspark@gmail.com>
2018-02-16 15:50:05 +0200

    Hot-fix

--- e2e/common/spectron-utils.ts
+++ e2e/common/spectron-utils.ts
@@ -8,12 +8,14 @@ const chai = require('chai');
 chai.should();
 chai.use(chaiAsPromised);
 
+const suffix = process.platform === 'win32' ? '.exe' : '';
+
 export class SpectronUtils {
   public static app = new SpectronApplication({
     // Your electron path can be any binary
     // i.e for OSX an example path could be '/Applications/MyApp.app/Contents/MacOS/MyApp'
     // But for the sake of the example we fetch it from our node_modules.
-    path: 'build-artifacts/electron-angular-native 1.0.0.exe',
+    path: 'build-artifacts/electron-angular-native 1.0.0' + suffix,
     args: [path.join(__dirname, '../..')],
   });
 
