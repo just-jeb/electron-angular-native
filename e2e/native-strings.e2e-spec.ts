cb9c40fb00928682d5de9f77b8fee56b50844338
Evgeny Barabanov <meltedspark@gmail.com>
2018-02-21 20:27:44 +0200

    Hopefully fix the test

--- e2e/native-strings.e2e-spec.ts
+++ e2e/native-strings.e2e-spec.ts
@@ -12,22 +12,15 @@ chai.use(chaiAsPromised);
 function containsString(app: Application, str: string) {
   it(`Contains ${str}`,
     () => {
-      app.webContents.savePage(path.resolve(__dirname, 'page.html'), 'HTMLComplete')
-        .then(function () {
-          console.log(fs.readFileSync(path.resolve(__dirname, 'page.html'), 'utf8'));
-        }).catch(function (error) {
-        console.error('saving page failed', error.message)
-      });
-    return app.client.waitUntilWindowLoaded().getText('ul li h2').then(list => {
+    return app.client.waitForExist('ul li h2').then(() => app.client.getText('ul li h2').then(list => {
         const index = list.indexOf(str);
         return index > -1 ? list[index] : '';
-      }).should.eventually.equal(str);
+      }).should.eventually.equal(str));
     }
   );
 }
 
 SpectronUtils.describe('Native strings', app => {
-
   containsString(app, 'c++ .node string');
   containsString(app, 'c++ precompiled library string');
 });
