a0299afc943c57fbe7e85be3b8f9955c7e34fae1
Evgeny Barabanov <meltedspark@users.noreply.github.com>
2018-02-23 14:11:59 +0200

    Add test for existence of all the native strings (#15)
    * Add test for existence of all the native strings
    
    * Fix appveyor.yml to avoid conflict of nodejs platform variable with system platform variable

--- e2e/common/spectron-utils.ts
+++ e2e/common/spectron-utils.ts
@@ -16,17 +16,13 @@ const platformToExtension = {
 
 export class SpectronUtils {
   public static app = new SpectronApplication({
-    // Your electron path can be any binary
-    // i.e for OSX an example path could be '/Applications/MyApp.app/Contents/MacOS/MyApp'
-    // But for the sake of the example we fetch it from our node_modules.
     path: `build-artifacts/ElectronAngularNativeApp.${platformToExtension[process.platform]}`,
-    args: [path.join(__dirname, '../..')],
   });
 
   static describe(desc: string, describeFunction: (app: Application) => void) {
 
     describe(desc, function () {
-      this.timeout(20000);
+      this.timeout(10000);
 
       before(() => {
         return SpectronUtils.app.start();
---
+++ e2e/native-strings.e2e-spec.ts
@@ -0,0 +1,26 @@
+import {SpectronUtils} from './common/spectron-utils';
+import {Application} from 'spectron';
+import * as path from 'path';
+import * as fs from 'fs';
+
+const chaiAsPromised = require('chai-as-promised');
+const chai = require('chai');
+
+chai.should();
+chai.use(chaiAsPromised);
+
+function containsString(app: Application, str: string) {
+  it(`Contains ${str}`,
+    () => {
+    return app.client.waitForExist('ul li h2').then(() => app.client.getText('ul li h2').then(list => {
+        const index = list.indexOf(str);
+        return index > -1 ? list[index] : '';
+      }).should.eventually.equal(str));
+    }
+  );
+}
+
+SpectronUtils.describe('Native strings', app => {
+  containsString(app, 'c++ .node string');
+  containsString(app, 'c++ precompiled library string');
+});
