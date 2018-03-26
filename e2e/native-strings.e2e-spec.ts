43ceda9b634be178be09371ac268ff675dadeef6
Evgeny Barabanov <meltedspark@users.noreply.github.com>
2018-03-26 16:14:43 +0300

    Fix title element path

--- e2e/native-strings.e2e-spec.ts
+++ e2e/native-strings.e2e-spec.ts
@@ -10,7 +10,7 @@ chai.use(chaiAsPromised);
 function containsString(app: Application, str: string) {
   it(`Contains ${str}`,
     () => {
-    return app.client.waitForExist('ul li h2').then(() => app.client.getText('ul li h2').then(list => {
+    return app.client.waitForExist('ul li h2 a').then(() => app.client.getText('ul li h2 a').then(list => {
         const index = list.indexOf(str);
         return index > -1 ? list[index] : '';
       }).should.eventually.equal(str));
@@ -19,6 +19,6 @@ function containsString(app: Application, str: string) {
 }
 
 SpectronUtils.describe('Native strings', app => {
-  containsString(app, 'c++ .node string');
-  containsString(app, 'c++ precompiled library string');
+  containsString(app, 'The link is provided by Node addon (using nan)');
+  containsString(app, 'The link is provided by precompiled native library (using ffi)');
 });
