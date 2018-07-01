a8a8561d2f0a12348ec6c452d961485b45abfed0
Evgeny Barabanov <meltedspark@gmail.com>
2018-07-01 18:30:23 +0300

    Use move custom-builders to a separate library

--- custom-builders/src/custom-webpack-browser/schema.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Evgeny Barabanov on 01/07/2018.
- */
-
-import {BrowserBuilderSchema} from '@angular-devkit/build-angular';
-
-export interface CustomWebpackBrowserBuildSchema extends BrowserBuilderSchema {
-  webpackConfigPath: string;
-}
--- custom-builders/src/custom-webpack-server/schema.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Evgeny Barabanov on 01/07/2018.
- */
-
-import {BuildWebpackServerSchema} from '@angular-devkit/build-angular/src/server/schema';
-
-export interface CustomWebpackServerBuildSchema extends BuildWebpackServerSchema {
-  webpackConfigPath: string;
-}
