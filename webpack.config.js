7aa2521422f555eb564ec6e5a536529fb974c3b1
Evgeny Barabanov <meltedspark@gmail.com>
2018-07-01 09:59:12 +0300

    Make ng build pass

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,30 @@ const platform = process.env.PLATFORM || process.platform;
 console.log(arch, platform);
 
 module.exports = {
+  externals: {
+    electron: "require('electron')",
+    buffer: "require('buffer')",
+    child_process: "require('child_process')",
+    crypto: "require('crypto')",
+    events: "require('events')",
+    fs: "require('fs')",
+    http: "require('http')",
+    https: "require('https')",
+    assert: "require('assert')",
+    dns: "require('dns')",
+    net: "require('net')",
+    os: "require('os')",
+    path: "require('path')",
+    querystring: "require('querystring')",
+    readline: "require('readline')",
+    repl: "require('repl')",
+    stream: "require('stream')",
+    string_decoder: "require('string_decoder')",
+    url: "require('url')",
+    util: "require('util')",
+    zlib: "require('zlib')",
+    ffi: "require('ffi')"
+  },
   module: {
     rules: [
       {
